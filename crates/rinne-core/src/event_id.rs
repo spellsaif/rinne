664794b95b2b9801728121ece28710240d3895d9
Saif Ali <this.saif.email@gmail.com>
2026-06-16 23:59:04 +0530

    feat: create helper methods to convert event_id as bytes or from bytes

--- crates/rinne-core/src/event_id.rs
+++ crates/rinne-core/src/event_id.rs
@@ -7,4 +7,14 @@ impl EventId {
     pub fn new() -> Self {
         Self(Uuid::now_v7())
     }
+
+    pub fn as_bytes(&self) -> &[u8;16] {
+        self.0.as_bytes()
+    }
+
+    pub fn from_bytes(
+        bytes: [u8;16]
+    ) -> Self {
+        Self(uuid::Uuid::from_bytes(bytes))
+    }
 }
