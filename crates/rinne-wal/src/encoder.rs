572304adb739ef64d0aafa451a1eb9a7f4cb3cf3
Saif Ali <this.saif.email@gmail.com>
2026-06-17 00:24:44 +0530

    fix: encoder was encoding tags value incorrectly

--- crates/rinne-wal/src/encoder.rs
+++ crates/rinne-wal/src/encoder.rs
@@ -1,74 +1,48 @@
-use bytes:: {BufMut, BytesMut};
+use bytes::{BufMut, BytesMut};
 
 use rinne_core::Event;
 
 use crate::WalError;
 
-pub fn encode(
-    event: &Event
-) -> Result<Vec<u8>, WalError> {
+pub fn encode(event: &Event) -> Result<Vec<u8>, WalError> {
     let mut buf = BytesMut::new();
 
     //UUID
-    buf.extend_from_slice(
-        event.id.as_bytes()
-    );
+    buf.extend_from_slice(event.id.as_bytes());
 
     // timestamp
-    buf.put_i64(
-        event.timestamp.value()
-    );
+    buf.put_i64(event.timestamp.value());
 
     // priority
-    buf.put_u8(
-        event.priority.to_u8()
-    );
+    buf.put_u8(event.priority.to_u8());
 
     // event type
     let event_type = event.event_type.as_str();
 
-    buf.put_u16(
-        event_type.len() as u16
-    );
+    buf.put_u16(event_type.len() as u16);
 
-    buf.extend_from_slice(
-        event_type.as_bytes()
-    );
+    buf.extend_from_slice(event_type.as_bytes());
 
     // Tags count
-    buf.put_u16(
-      event.tags.len() as u16
-    );
+    buf.put_u16(event.tags.len() as u16);
 
     // Tags
     for (key, value) in &event.tags {
         // Key
-        buf.put_u16(
-            key.len() as u16
-        );
+        buf.put_u16(key.len() as u16);
 
-        buf.extend_from_slice(
-            key.as_bytes()
-        );
+        buf.extend_from_slice(key.as_bytes());
 
         // value
-        buf.put_u16(
-            value.len() as u16
-        );
+        buf.put_u16(value.len() as u16);
 
-        buf.extend_from_slice(
-            key.as_bytes()
-        );
+        buf.extend_from_slice(value.as_bytes());
     }
 
     // Payload
-    buf.put_u32(
-        event.payload.len() as u32
-    );
+    buf.put_u32(event.payload.len() as u32);
 
-    buf.extend_from_slice(
-        &event.payload
-    );
+    buf.extend_from_slice(&event.payload);
 
     Ok(buf.to_vec())
 }
