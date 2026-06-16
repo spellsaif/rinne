8545ffc737517c957024bf86f8999cf9c24e546d
Saif Ali <this.saif.email@gmail.com>
2026-06-17 00:01:56 +0530

    util: create uility methods to convert to u8 or from u8

--- crates/rinne-core/src/priority.rs
+++ crates/rinne-core/src/priority.rs
@@ -4,3 +4,22 @@ pub enum Priority {
     Normal,
     High
 }
+
+impl Priority {
+    pub fn to_u8(self) -> u8 {
+        match self {
+            Self::Low => 0,
+            Self::Normal => 1,
+            Self::High => 2,
+        }
+    }
+
+    pub fn from_u8(value: u8) -> Option<Self> {
+        match value {
+            0 => Some(Self::Low),
+            1 => Some(Self::Normal),
+            2 => Some(Self::High),
+            _ => None,
+        }
+    }
+}
